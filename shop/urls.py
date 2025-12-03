60ae75ae78fd1818ea7ccd40f02f4297374f481f
Radwan <radwan@duck.com>
2025-12-03 16:10:32 +0200

    feat: added item_update view

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, item_views, user_auth, login, item_update
+from .views import home, item_views, user_create_view, login, item_update
 
 app_name = 'shop'
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('items/', item_views, name='items'),
     path('items/', item_update, name='items'),
-    path('users/', user_auth, name='users'),
-    path('login/', login, name='login'),
+    path('users/', user_create_view, name='users'),
+    path('users/login/', login, name='login'),
     
 ]
--- shop/views.py
+++ shop/views.py
@@ -10,7 +10,7 @@ def home(request):
 
 ############## Authentication Views:
 
-def user_auth(request):
+def user_create_view(request):
     user1, _ = User.objects.get_or_create(username='testuser', email='test@test.com', phone_number='+49333434343', birthdate='1990-12-12')
     #user1.save()
     user2, _ = User.objects.get_or_create(username='nextuser', email='testing@testing.com', phone_number='+4933', birthdate='1900-12-12')
@@ -34,4 +34,7 @@ def item_views(request):
     return HttpResponse('item added')
     
 def item_update(request):
-    
+    item = request.GET.get('name')
+    if item:
+        return HttpResponse('Found it')
+    return HttpResponse('What are you looking for? entry dont exist')
