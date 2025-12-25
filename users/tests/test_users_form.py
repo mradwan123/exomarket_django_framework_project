e5c68c204bfc01cce497c8843c47559b9ba14c53
Rad1 <muhammadradwan@Air-Malware.local>
2025-12-25 13:14:13 +0200

    test: added test_invalid_email_format

--- users/tests/test_users_form.py
+++ users/tests/test_users_form.py
@@ -21,3 +21,21 @@ class UserFormTests(TestCase):
         form = UserForm(data={})
         
         self.assertFalse(form.is_valid())
+
+    def test_invalid_email_format(self):
+        """
+        Provide an email that does not match Django's EmailValidator
+        """
+        data = {
+            "username":      "testerusername",
+            "email":         "bad-email_format",          # <-- deliberately bad
+            "password":      "test123",
+            "phone_number":  "222222222",
+            "bio":           "some text",
+            "birth_date":    "12/12/1976",
+        }
+
+        form = UserForm(data=data)
+        self.assertFalse(form.is_valid())
+
+        self.assertIn("email",form.errors)
