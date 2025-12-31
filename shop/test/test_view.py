729047d2d36ba3642c39f333e39ec83902f0e19e
Rad1 <muhammadradwan@Air-Malware.local>
2025-12-31 15:09:10 +0200

    test: added test to check if username displayed in homepage upon rendering home.html

--- shop/test/test_view.py
+++ shop/test/test_view.py
@@ -21,3 +21,12 @@ class SessionViewsTest(TestCase):
         
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
+        
+    def test_home_view_display_user(self):
+        login_ok = self.client.login(username="testuser", password="test123")
+        self.assertTrue(login_ok, "Login failed in test setup")
+
+        response = self.client.get(self.home_url)
+
+        self.assertIn("user", response.context)
+        self.assertEqual(response.context["user"], self.user.username)
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Item, Cart
 from .forms import ItemForm
 
 
 # Home view
+
 
 def home(request):
     
