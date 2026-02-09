c582d09d8bf8be94cf6ff0a73efc63569b33fcb7
Radwan <radwan@duck.com>
2026-02-09 16:15:39 +0200

    all heroku config files added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn exomarket.wsgi --log-file -
--- exomarket/settings.py
+++ exomarket/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'dry-forest-33364-2cb04b39d3d4.herokuapp.com']
 
 
 # Application definition
